1cd4c9d1ed7294c8d158f7684db4baf61a067351
Jemm77 <eduardo.marquez@cetys.edu.mx>
2025-10-06 15:40:16 -0700

    Main
    Primer comit del proyecto q emocion hay si voy por 3 de maciza con el guero pero en fa k ando bien crudote

---
+++ main/main/mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  mainApp.swift
+//  main
+//
+//  Created by CETYS Universidad  on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ main/mainTests/mainTests.swift
@@ -0,0 +1,17 @@
+//
+//  mainTests.swift
+//  mainTests
+//
+//  Created by CETYS Universidad  on 06/10/25.
+//
+
+import Testing
+@testable import main
+
+struct mainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
